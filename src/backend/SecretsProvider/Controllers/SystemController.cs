1f05e8b58a9d31eba05b238b73c9225bd70e5849
Andrey Kiselev <omega-faworit@yandex.ru>
2021-08-28 17:54:50 +0300

    feat: add system controller

---
+++ src/backend/SecretsProvider/Controllers/SystemController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using Ralfred.Modules;
+
+
+namespace Ralfred.Controllers
+{
+	[ApiController]
+	[Route("system")]
+	public class SystemController : ControllerBase
+	{
+		[HttpGet("status")]
+		public SystemInfo GetSystemInfo()
+		{
+			return new SystemInfo
+			{
+				Version = "0.0.0",
+				StorageType = "Inmemory"
+			};
+		}
+	}
+}
---
+++ src/backend/SecretsProvider/Modules/SystemInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Ralfred.Modules
+{
+	public class SystemInfo
+	{
+		public string Version { get; set; }
+
+		public string StorageType { get; set; }
+	}
+}
