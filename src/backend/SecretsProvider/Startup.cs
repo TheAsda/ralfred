ac7d53ec7dc6e5113d8b16738b3b8bc0a6404e54
Gubanov Ilya <yoyrandao@gmail.com>
2021-08-28 22:40:45 +0300

    refactor: refactored root namespace

--- src/backend/SecretsProvider/Startup.cs
+++ src/backend/SecretsProvider/Startup.cs
@@ -1,13 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Ralfred.Common.DependencyInjection;
 
 
-namespace Ralfred
+namespace Ralfred.SecretsProvider
 {
 	public class Startup
 	{
