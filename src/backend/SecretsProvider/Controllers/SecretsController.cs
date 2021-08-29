6c088b3c202b8c04d84b228b952d18db62a050c3
Andrey Kiselev <omega-faworit@yandex.ru>
2021-08-29 13:03:56 +0300

    feat: add secrets controller

---
+++ src/backend/SecretsProvider/Controllers/SecretsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Ralfred.Controllers
+{
+	[ApiController]
+	public class SecretsController : ControllerBase
+	{
+		[HttpPut("{*route}")]
+		// TODO: add body payload
+		public void AddSecrets([FromForm] IFormCollection formPayload, [FromQuery] string? secrets, [FromRoute] string route)
+		{
+			var secretNames = secrets?.Split(',');
+			var secretPath = route.Split("/");
+		}
+		
+		[HttpDelete("{*route}")]
+		// TODO: add body payload
+		public void RemoveSecrets([FromForm] IFormCollection formPayload, [FromQuery] string? secrets, [FromRoute] string route)
+		{
+			var secretNames = secrets?.Split(',');
+			var secretPath = route.Split("/");
+		}
+	}
+}
