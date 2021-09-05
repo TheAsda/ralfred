ea82faf16f567e832af299408c72fdfd0f88bcde
Gubanov Ilya <yoyrandao@gmail.com>
2021-09-05 13:48:57 +0300

    feat: removed secrets from group entity

--- src/backend/Common/DataAccess/Entities/Group.cs
+++ src/backend/Common/DataAccess/Entities/Group.cs
@@ -1,14 +1,9 @@
-﻿using System;
-
-
-namespace Ralfred.Common.DataAccess.Entities
+﻿namespace Ralfred.Common.DataAccess.Entities
 {
 	public record Group : Entity
 	{
 		public string Name { get; init; }
 
 		public string Path { get; init; }
-
-		public Secret[] Secrets { get; init; }
 	}
 }
