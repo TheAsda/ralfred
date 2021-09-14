f4280a0f0249d4dd0c7e673d37d29d22fe80b76f
Andrey Kiselev <omega-faworit@yandex.ru>
2021-09-15 00:53:15 +0300

    feat: add secret handling in secrets manager

--- src/backend/Common/Managers/SecretsManager.cs
+++ src/backend/Common/Managers/SecretsManager.cs
@@ -65,7 +65,10 @@ namespace Ralfred.Common.Managers
 				case PathType.None:
 				{
 					var (groupName, folderPath) = _pathResolver.DeconstructPath(path);
-					_groupRepository.CreateGroup(groupName, folderPath ?? "", input, files);
+
+					_groupRepository.CreateGroup(groupName, folderPath ?? "",
+						FilterDictionaryKeys(input, secrets),
+						FilterDictionaryKeys(files, secrets));
 
 					break;
 				}
@@ -73,11 +76,14 @@ namespace Ralfred.Common.Managers
 				{
 					var (groupName, folderPath) = _pathResolver.DeconstructPath(path);
 
+					// NOTE: Возможно поведение непонятное, так как в кейсе None производится фильтрация всегда и устанавливаются секреты из
+					// списка, а в данном кейсе при наличии фильтра производится замена выбранных, в обратном случае заменяются все секреты 
+					// на предоставленные в словаре.
 					if (secrets.Length > 0)
 					{
 						_secretsRepository.UpdateGroupSecrets(groupName, folderPath ?? "",
-							input.Where(x => secrets.Contains(x.Key)).ToDictionary(x => x.Key, x => x.Value),
-							files.Where(x => secrets.Contains(x.Key)).ToDictionary(x => x.Key, x => x.Value));
+							FilterDictionaryKeys(input, secrets),
+							FilterDictionaryKeys(files, secrets));
 					}
 					else
 					{
@@ -86,10 +92,40 @@ namespace Ralfred.Common.Managers
 
 					break;
 				}
+				case PathType.Secret:
+				{
+					if (!input.ContainsKey("value") && !files.ContainsKey("value"))
+					{
+						throw new Exception("Value is not provided");
+					}
+
+					var (name, groupPath) = _pathResolver.DeconstructPath(path);
+					var (groupName, folderPath) = _pathResolver.DeconstructPath(groupPath);
+
+					if (input.ContainsKey("value"))
+					{
+						_secretsRepository.UpdateGroupSecrets(groupName, folderPath ?? "",
+							new Dictionary<string, string> { { name, input["value"] } },
+							new Dictionary<string, string>());
+					}
+					else
+					{
+						_secretsRepository.UpdateGroupSecrets(groupName, folderPath ?? "",
+							new Dictionary<string, string>(),
+							new Dictionary<string, string> { { name, files["value"] } });
+					}
+
+					break;
+				}
 				default:
 					throw new ArgumentOutOfRangeException();
 			}
 		}
+
+		private Dictionary<string, string> FilterDictionaryKeys(Dictionary<string, string> dictionary, IEnumerable<string> keys) =>
+			dictionary
+				.Where(x => !keys.Any() || keys.Contains(x.Key))
+				.ToDictionary(x => x.Key, x => x.Value);
 
 		private readonly IPathResolver _pathResolver;
 		private readonly ISecretsRepository _secretsRepository;
