d148cdd73c30b7862ff173041c90a51845cf1b9a
Andrey Kiselev <omega-faworit@yandex.ru>
2021-10-30 22:33:08 +0300

    feat: add uuid migration

---
+++ src/backend/Common/DataAccess/Repositories/Postgres/Migrations/UuidMigration.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator;
+
+
+namespace Ralfred.Common.DataAccess.Repositories.Postgres.Migrations
+{
+	[Migration(0)]
+	public class UuidMigration : ForwardOnlyMigration
+	{
+		public override void Up()
+		{
+			Execute.Sql("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"");
+		}
+	}
+}
