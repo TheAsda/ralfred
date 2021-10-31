ec90ad130a57b516a0b1d7f9c1e810568b6c6d98
Gubanov Ilya <yoyrandao@gmail.com>
2021-10-31 13:19:46 +0300

    feat: configured integration tests to run migration, added new pipeline base

---
+++ src/backend/Common.IntegrationTests/Setup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using FluentMigrator.Runner;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Ralfred.Common.Types;
+
+
+namespace Common.IntegrationTests
+{
+	public class Setup
+	{
+		public static IServiceProvider CreateServices()
+		{
+			var referencedAssembly = Assembly.Load("Ralfred.Common");
+
+			return new ServiceCollection()
+				.AddFluentMigratorCore()
+				.ConfigureRunner(rb =>
+				{
+					rb.AddPostgres()
+						.WithGlobalConnectionString(ConfigurationHelper.PostgresDatabaseConnectionString)
+						.ScanIn(referencedAssembly).For.Migrations();
+				})
+				.BuildServiceProvider(false);
+		}
+	}
+}
---
+++ src/backend/Common.IntegrationTests/SetupFixture.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator.Runner;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using NUnit.Framework;
+
+
+namespace Common.IntegrationTests
+{
+	[SetUpFixture]
+	public class SetupFixture
+	{
+		[OneTimeSetUp]
+		public void GlobalSetup()
+		{
+			var serviceProvider = Setup.CreateServices();
+			var postgresMigrationRunner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+			postgresMigrationRunner.MigrateUp();
+		}
+	}
+}
